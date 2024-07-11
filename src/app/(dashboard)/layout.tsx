93a09c83d3622405d14b8b34f88b5aa570883421
dasorioano <soriano.daleandrew@gmail.com>
2024-07-11 17:55:36 +0800

    feat: update

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,4 +1,5 @@
 import Sidebar from "@/components/SideBar";
+import Navbar from "@/components/NavBar";
 
 export default function RootLayout({
   children,
@@ -8,7 +9,10 @@ export default function RootLayout({
   return (
     <div className="flex">
       <Sidebar />
-      <main className="flex-auto">{children}</main>
+      <main className="flex-auto">
+        <Navbar />
+        {children}
+        </main>
     </div>
   );
 }
