2dd37a8a17549fd2d94512449c8aed635b435162
Louisseley <louisseangelosapaula@gmail.com>
2024-07-12 17:49:55 +0800

    feat: edit profile modal

--- src/app/(dashboard)/transaction/page.tsx
+++ src/app/(dashboard)/transaction/page.tsx
@@ -2,44 +2,11 @@
 import { useState, useEffect } from "react"
 // import axios from "axios"
 import TransactionRightSection from "./TransactionRightSection";
-import Frame16 from "./AddNewModal";
 const Transaction = () => {
-  const [addNew, setAddNew] = useState(false);//TOggle the add new section
-  const [addressButton, setAddressButton] = useState(false);//Toggle service option
-  const [serviceButton, setServiceButton] = useState(false);//TOggle services option
-  // const [dataValue, setDataValue] = useState([]);//Storage of the datapen
-  const [importModalOpen, setImportModalOpen] = useState(false);//Toggle import modal
-
-// useEffect(() => {
-//   axios.get('http://localhost:3500/users')
-//   .then(res => setDataValue(res.data))
-//   .catch(err => console.log(err))
-// })
-
-  const toggleAddressButton = () => {
-    setAddressButton(!addressButton);
-  };
-  
-  const toggleServiceButton = () => {
-    setServiceButton(!serviceButton);
-  };
-
-  const addNewClick = () => {
-    setAddNew(!addNew);
-    setAddressButton(false);
-    setServiceButton(false);
-  };
+ 
   return (
     <div className="flex">
-      <TransactionRightSection
-      addNewClick={addNewClick}/>
-      <Frame16 
-        addNew={addNew}
-        addNewClick={addNewClick}
-        toggleServiceButton={toggleServiceButton}
-        addressButton={addressButton}
-        serviceButton={serviceButton}
-      />
+      <TransactionRightSection/>
     </div>
   );
 };
