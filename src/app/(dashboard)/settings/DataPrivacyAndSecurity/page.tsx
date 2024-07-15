f908839f314697e1b298b2433f3f02a3c6ee05b1
kysm5 <layosamikel@gmail.com>
2024-07-15 17:41:47 +0800

    fix(Settings): Updated Color Scheme UI and added details in Data Privacy settings:(added two folders and pages password and data protection,data privacy policy).

--- src/app/(dashboard)/settings/DataPrivacyAndSecurity/page.tsx
+++ src/app/(dashboard)/settings/DataPrivacyAndSecurity/page.tsx
@@ -1,9 +1,8 @@
-import { Header } from '@/components/Settings/Header';
-
 export default function DataPrivacyAndSecurity() {
+
   return (
     <>
-      <Header category={'Data Privacy and Security'} label={'Lorem ipsum dolor sit amet, consectetur adipiscing elit'} />
+      <text className="text-sm text-black">Data Privacy And Security</text>
     </>
   );
 }
